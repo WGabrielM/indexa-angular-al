df0cf5bc045bda242d89b3bb794ccb53fe67b020
Wellington <wellingtongmb@gmail.com>
2024-10-03 19:12:14 -0300

    create contact component and fix header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./components/container/container.component";
 import { HeaderComponent } from './components/header/header.component';
 import { SplitterComponent } from "./components/splitter/splitter.component";
+import { ContactComponent } from "./components/contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, ContainerComponent, HeaderComponent, SplitterComponent],
+  imports: [RouterOutlet, ContainerComponent, ContainerComponent, HeaderComponent, SplitterComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
