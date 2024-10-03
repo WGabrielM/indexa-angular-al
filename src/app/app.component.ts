81e5c836fba156e75170ef4227e5ec6660082af6
Wellington <wellingtongmb@gmail.com>
2024-10-03 18:52:25 -0300

    create header and splitter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from "./components/container/container.component";
+import { HeaderComponent } from './components/header/header.component';
+import { SplitterComponent } from "./components/splitter/splitter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerComponent, ContainerComponent, HeaderComponent, SplitterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alphabet: string = 'abcdefghijklmnopqrstuvwxyz'
 }
